44822ef90a27ced3afe91bd61818c6c166959f53
arthurhenz <arthur.henz@edu.pucrs.br>
2022-05-28 06:25:42 -0300

    card sem cor

--- frontend/creativdev-front/src/App.js
+++ frontend/creativdev-front/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
 import FormularioDoador from './components/FormularioDoador';
 import {Homescreen} from './screens/Homescreen'
-import Card from './components/Card'
+import {Card} from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <Card />
-
     </div>
   );
 }
