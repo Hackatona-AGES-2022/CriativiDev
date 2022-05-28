c0170af6f485feb3df0a9280266ad66a5f4a20b8
roselaine-moreira <roselaine.moreira@edu.pucrs.br>
2022-05-28 07:44:57 -0300

    Tudo classes

--- frontend/creativdev-front/src/screens/login/TelaLogin.jsx
+++ frontend/creativdev-front/src/screens/login/TelaLogin.jsx
@@ -2,19 +2,21 @@ import Logo from "../../assets/LOGO.png"
 import "./style.css";
 
 export const TelaLogin = () => {return(
-    <div className="TelaLogin">
-       <div class="container-img">
-            <img src={Logo} alt="" />
+    <div className="container-fluid TelaLogin">
+       <main className="minhaMain">
+       <div class="minhaContainer-img">
+            <img className="minha-img" src={Logo} alt="" />
         </div>
-        <form action="#">
-            <label  for="email">E-mail</label>
+        <form className="meu-form" action="#">
+            <label className="minhalabel" for="email">E-mail</label>
             <input type="email"  id="email" class="entrada" placeholder="Digite seu e-mail" />
-            <label for="senha">Senha</label>
+            <label className="minhalabel" for="senha">Senha</label>
             <input type="password" id="senha" class="entrada" placeholder="Digite seu e-mail" />
             <p><a href="http://">Não possui conta ainda?</a></p>
-            <input type="submit" value="Entrar" class="btn submit" />
-            <input type="button" value="Voltar" class="btn back" /> 
+            <input type="submit" value="Entrar" class="meu-btn submit" />
+            <input type="button" value="Voltar" class="meu-btn back" /> 
         </form>
+       </main>
     </div>
   );
 } 
