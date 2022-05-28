cd329abe06d74688221fddbc5b13aa9a5028e0bf
arthurhenz <arthur.henz@edu.pucrs.br>
2022-05-28 04:29:02 -0300

    header pronto

--- frontend/creativdev-front/src/App.js
+++ frontend/creativdev-front/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import FormularioDoador from './components/FormularioDoador';
-import Header from './containers/Header'
+import {HeaderReciever, HeaderDonator, HeaderLogin} from './containers/Header'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <HeaderLogin />
 
     </div>
   );
